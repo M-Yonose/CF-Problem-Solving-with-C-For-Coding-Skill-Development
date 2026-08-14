369ec60586083d24c5ba379fa591f588c6c69807
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-14 21:55:36 +0600

    CF_282_A_Solve_with_C#

---
+++ SourceCode/CF_282_A/Program.cs
@@ -0,0 +1,28 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+task_02_Codeforces_282A*/
+using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int x = 0;
+        string s;
+        for (int i = 0; i < n; i++)
+        {
+            s = Console.ReadLine();
+            if (s == "X++" || s == "++X")
+            {
+                x++;
+            }
+            else if (s == "X--" || s == "--X")
+            {
+                x--;
+            }
+        }
+        Console.WriteLine(x);
+
+    }
+}
