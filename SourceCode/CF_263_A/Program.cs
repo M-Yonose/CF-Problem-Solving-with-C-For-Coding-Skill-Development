f2be7f79f26c43e65d3233d9b5785d9ec7eb856a
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-16 00:20:38 +0600

    CF_263_A_Solve_with_C#

---
+++ SourceCode/CF_263_A/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_03_Codeforces_263A
+*/
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            for (int j = 0; j < 5; j++)
+            {
+                int x = int.Parse(input[j]);
+
+                if (x == 1)
+                {
+                    Console.WriteLine(Math.Abs(i - 2) + Math.Abs(j - 2));
+                    return;
+                }
+            }
+        }
+    }
+}
