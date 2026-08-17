16f7d5b098e6693dd4b73f7f9b8a03785375cbb7
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-17 11:29:34 +0600

    CF_112_A_Solved_with_C#

---
+++ SourceCode/CF_112_A/Program.cs
@@ -0,0 +1,29 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+task_04_Codeforces_112_A
+*/
+using System;
+class Programme
+{
+    static void Main()
+    {
+        string a = Console.ReadLine().ToUpper();
+        string b = Console.ReadLine().ToUpper();
+
+        int result = string.Compare(a, b);
+
+        if (result > 0)
+        {
+            Console.WriteLine("1");
+        }
+        else if(result <0)
+        {
+            Console.WriteLine("-1");
+        }
+        else
+        {
+            Console.WriteLine("0");
+        }
+    }
+}
