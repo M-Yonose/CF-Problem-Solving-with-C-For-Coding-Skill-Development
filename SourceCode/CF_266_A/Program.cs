02bd5a606d9892e3371178f1a40e428d2931ae6f
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-18 00:09:07 +0600

    CF_266_A_Solved_with_C#

---
+++ SourceCode/CF_266_A/Program.cs
@@ -0,0 +1,24 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_06_Codeforces_266_A
+*/
+using System;
+class Programme
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        string stones = Console.ReadLine();
+        int count = 0;
+
+        for(int i = 0; i < n - 1; i++)
+        {
+            if (stones[i] == stones[i+1])
+            {
+                count++;
+            }
+        }
+        Console.WriteLine(count);
+    }
+}
