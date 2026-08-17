81e37eec5a177ce369ca275bf7a1ec876fbb6206
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-17 12:07:24 +0600

    CF_118_A_Solved_with_C#

---
+++ SourceCode/CF_118_A/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+task_05_Codeforces_118_A
+*/
+
+using System;
+class Programme
+{
+    static void Main()
+    {
+        string input = Console.ReadLine().ToLower();
+        for ( int i= 0; i< input.Length; i++ )
+        {
+            char ch = input[i];
+            if ( ch == 'a' || ch == 'o' || ch == 'y' || ch == 'e' || ch == 'i' || ch == 'u' )
+            {
+
+            }
+            else
+            {
+                Console.Write("." + ch);
+            }
+        }
+    }
+}
