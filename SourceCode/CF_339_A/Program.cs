bd2087a4a617d615773cbd445bc9f743694d61c8
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-17 21:55:44 +0600

    CF_339_A_Solved_with_C#

---
+++ SourceCode/CF_339_A/Program.cs
@@ -0,0 +1,18 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_05_Codeforces_339A
+*/
+using System;
+class Programme
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+        string[] numbers = s.Split('+');
+
+        Array.Sort(numbers);
+        Console.WriteLine(string.Join("+", numbers));
+
+    }
+}
