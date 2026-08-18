c751fb0af59f6122a5f33e60e743a5e17f8f0a9a
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-18 23:50:02 +0600

    CF_236_A_Solved_with_C# CF_96_A_Solved_with_C#

---
+++ SourceCode/CF_236_A/Program.cs
@@ -0,0 +1,44 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_08_Codeforces_236_A
+*/
+
+using System;
+
+class Programme
+{
+    static void Main()
+    {
+        string name = Console.ReadLine();
+        int count = 0;
+
+        for (int i = 0; i < name.Length; i++)
+        {
+            bool isDuplicate = false;
+
+            for (int j = 0; j < i; j++)
+            {
+                if (name[i] == name[j])
+                {
+                    isDuplicate = true;
+                    break;
+                }
+            }
+
+            if (!isDuplicate)
+            {
+                count++;
+            }
+        }
+
+        if (count % 2 == 0)
+        {
+            Console.WriteLine("CHAT WITH HER!");
+        }
+        else
+        {
+            Console.WriteLine("IGNORE HIM!");
+        }
+    }
+}
---
+++ SourceCode/CF_96_A/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_09_Codeforces_96_A
+*/
+
+using System;
+
+class Programme
+{
+    static void Main()
+    {
+        string players = Console.ReadLine();
+
+        int count = 1;
+
+        for (int i = 1; i < players.Length; i++)
+        {
+            if (players[i] == players[i - 1])
+            {
+                count++;
+
+                if (count >= 7)
+                {
+                    Console.WriteLine("YES");
+                    return;
+                }
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+
+        Console.WriteLine("NO");
+    }
+}
