36c4ac12b797b8d02bf4ea9a38c8b521f972d5fc
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-23 18:14:23 +0600

    CF_791_A_Solved_with_C#

---
+++ SourceCode/CF_791_A/Program.cs
@@ -0,0 +1,30 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_11_Codeforces_791_A
+*/
+
+using System;
+
+class Programme
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+
+        int a = int.Parse(input[0]);
+        int b = int.Parse(input[1]);
+
+        int year = 0;
+
+        while (a <= b)
+        {
+            a = a * 3;
+            b = b * 2;
+
+            year++;
+        }
+
+        Console.WriteLine(year);
+    }
+}
