d556c76865c4e27b31f45df5fc01cfa153d313c0
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-24 12:24:40 +0600

    CF_617_A_SOLVED_WITH_C#

---
+++ SourceCode/CF_617_A/Program.cs
@@ -0,0 +1,31 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_13_Codeforces_617_A
+*/
+
+using System;
+
+class Programme
+{
+   static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int count = 0;
+
+        int ans = 0;
+
+        if(n % 5 == 0)
+        {
+            ans = n / 5;
+        }
+        else
+        {
+            ans = (n / 5) + 1;
+        }
+
+        Console.WriteLine(ans);      
+
+    }
+}
