601bef76cd9c9d663b8cbf02cbd6c117996e40bf
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-24 12:38:54 +0600

    CF_59_A_SOLVED_WITH_C#

---
+++ SourceCode/CF_59_A/Program.cs
@@ -0,0 +1,37 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_14_Codeforces_59_A
+*/
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        string word = Console.ReadLine();
+
+        int upper = 0;
+        int lower = 0;
+
+        for( int i = 0; i < word.Length; i++ )
+        {
+            if (char.IsUpper(word[i]))
+            {
+                upper++;
+            }
+            else
+            {
+                lower++;
+            }
+        }
+        if(upper > lower)
+        {
+            Console.WriteLine(word.ToUpper());
+        }
+        else
+        {
+            Console.WriteLine(word.ToLower());
+        }
+    }
+}
