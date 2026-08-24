c611610c79c8d1e8e2d0a74d1949d38d80440771
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-24 15:06:29 +0600

    CF_133_A_SOLVED_WITH_C#

---
+++ SourceCode/CF_133_A/Program.cs
@@ -0,0 +1,26 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_17_Codeforces_133_A
+*/
+
+using System;
+
+class Programme
+{
+    static void Main()
+    {
+        string s = Console.ReadLine();
+
+        for(int i = 0; i < s.Length; i++)
+        {
+            if(s[i] == 'H' || s[i] == 'Q' || s[i] == '9')
+            {
+                Console.WriteLine("YES");
+                return;
+            }
+        }        
+
+        Console.WriteLine("NO");
+    }
+}
