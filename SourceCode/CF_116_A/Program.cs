b411af83ab53d61c53ac9db6e92026dba195c7f0
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-24 14:23:52 +0600

    CF_116_A_SOLVED_WITH_C#

---
+++ SourceCode/CF_116_A/Program.cs
@@ -0,0 +1,33 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_15_Codeforces_116_A
+*/
+
+using System;
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+
+        int current = 0;
+        int max = 0;
+
+        for(int i = 0; i < n; i++)
+        {
+            string[] input = Console.ReadLine().Split();
+
+            int exit = int.Parse(input[0]);
+            int enter = int.Parse(input[1]);
+
+            current = current - exit + enter;
+
+            if(current > max)
+            {
+                max = current;
+            }
+        }
+        Console.WriteLine(max);
+    }
+}
