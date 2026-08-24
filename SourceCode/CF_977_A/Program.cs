76b7d8436ac23303b72b4fc739e2cc51e552fbea
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-24 11:27:45 +0600

    CF_977_A_Solved_With_C#

---
+++ SourceCode/CF_977_A/Program.cs
@@ -0,0 +1,32 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_12_Codeforces_977_A
+*/
+
+using System;
+
+class Programme
+{
+    static void Main()
+    {
+        string[] input = Console.ReadLine().Split();
+
+        int n = int.Parse(input[0]);
+        int k = int.Parse(input[1]);     
+
+        for (int i = 0; i < k; i++)
+        {   
+            if(n % 10 == 0)
+            {
+                n = n / 10;
+            }
+            else
+            {
+                n = n - 1;
+            }
+        }
+        Console.WriteLine(n);
+
+    }
+}
