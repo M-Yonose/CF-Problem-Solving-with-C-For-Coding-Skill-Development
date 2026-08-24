4c0ae38f45366d5f525566c26b7eca1e271a0e12
Mohammed Yonose <mdyonose1999@gmail.com>
2026-08-24 14:48:08 +0600

    CF_58_A_SOLVED_WITH_C#

---
+++ SourceCode/CF_58_A/Program.cs
@@ -0,0 +1,35 @@
+﻿/*Bismillahir Rahmanir Rahim
+Mohammed Yonose
+100 CF Problems For Coding Skill Development
+Task_16_Codeforces_58_A
+*/
+
+using System;
+
+class Programme
+{
+    static void Main()
+    {
+        string word = Console.ReadLine();
+
+        string target = "hello";
+        int count = 0;
+
+        for (int i = 0; i < word.Length; i++)
+        {
+            if (count < target.Length && word[i] == target[count])
+            {
+                count++;
+            }
+        }
+
+        if (count == target.Length)
+        {
+            Console.WriteLine("YES");
+        }
+        else
+        {
+            Console.WriteLine("NO");
+        }
+    }
+}
